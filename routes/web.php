6ec4d843ce2ad52bc6d1a935b31c6419566e0a00
ahmedhi <ahmedhilali07@gmail.com>
2019-07-20 15:00:07 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('connexion',)
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
